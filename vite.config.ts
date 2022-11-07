4b7271c6e95a6ca7f8a2c28c53455626dfde0699
smyzin <smyzin.work@gmail.com>
2022-11-08 01:42:33 +0300

    Update vite.conf

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig, Plugin } from 'vite'
 import preact from '@preact/preset-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { visualizer } from 'rollup-plugin-visualizer'
-import GlobalsPolyfills from '@esbuild-plugins/node-globals-polyfill'
-import nodePolyfills from 'rollup-plugin-polyfill-node'
 
 export default defineConfig({
   plugins: [preact(), tsconfigPaths()],
@@ -13,25 +11,9 @@ export default defineConfig({
         visualizer({
           gzipSize: true,
           brotliSize: true,
-        }),
-        nodePolyfills(),
-      ] as unknown[] as Plugin[],
-    },
-    outDir: './docs',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [
-        GlobalsPolyfills({
-          buffer: true,
-        }),
+        }) as Plugin,
       ],
     },
-  },
-  esbuild: {
-    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+    outDir: './docs',
   },
 })
