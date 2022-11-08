8e2289578db596128cca7a545e933d0c1bfb0a3b
smyzin <smyzin.work@gmail.com>
2022-11-08 15:55:26 +0300

    Fix status block card

--- src/components/StatusBlock.tsx
+++ src/components/StatusBlock.tsx
@@ -15,7 +15,7 @@ import classnames, {
 
 const statusLoading = classnames(
   display('flex'),
-  backgroundColor('bg-primary-dark'),
+  backgroundColor('bg-primary-background'),
   flexDirection('flex-col'),
   padding('p-6'),
   alignItems('items-center'),
