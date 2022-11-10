3da0441ea0aec0fbb192abf75c04b1c7fff77678
smyzin <smyzin.work@gmail.com>
2022-11-10 17:07:13 +0300

    Small fix

--- src/helpers/signMessage.ts
+++ src/helpers/signMessage.ts
@@ -1,9 +1,5 @@
 import { Signer } from 'ethers'
 import { hashPersonalMessage } from '@ethereumjs/util'
-import buffer from 'buffer'
-
-const { Buffer } = buffer
-if (!window.Buffer) window.Buffer = Buffer
 
 export default async function (address: string, signer: Signer) {
   const baseMessage = `SealHub verification for ${address}`
