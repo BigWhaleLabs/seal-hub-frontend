e7047bb4e37d3010c733e99187e9d3eb4dfe0b12
smyzin <smyzin.work@gmail.com>
2022-11-10 16:15:09 +0300

    Change path to dir

--- scripts/getWasm.sh
+++ scripts/getWasm.sh
@@ -1,3 +1,3 @@
-curl https://bwl-zk.s3.amazonaws.com/ECDSAChecker.wasm -o public/zkp/ECDSAChecker.wasm
-curl https://bwl-zk.s3.amazonaws.com/ECDSAChecker_final.zkey -o public/zkp/ECDSAChecker_final.zkey
-curl https://bwl-zk.s3.amazonaws.com/ECDSAChecker_verification_key.json -o public/zkp/ECDSAChecker_verification_key.json
+curl https://bwl-zk.s3.amazonaws.com/ECDSAChecker.wasm -o public/zk/ECDSAChecker.wasm
+curl https://bwl-zk.s3.amazonaws.com/ECDSAChecker_final.zkey -o public/zk/ECDSAChecker_final.zkey
+curl https://bwl-zk.s3.amazonaws.com/ECDSAChecker_verification_key.json -o public/zk/ECDSAChecker_verification_key.json
