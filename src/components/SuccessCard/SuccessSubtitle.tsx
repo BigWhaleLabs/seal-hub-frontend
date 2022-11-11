1cedbfaa086b456695e1113b1c8fdf5dac711fc5
smyzin <smyzin.work@gmail.com>
2022-11-11 15:17:35 +0300

    Reset connection

--- src/components/SuccessCard/SuccessSubtitle.tsx
+++ src/components/SuccessCard/SuccessSubtitle.tsx
@@ -1,21 +1,22 @@
 import { LinkText } from 'components/Text'
+import { STAGE } from 'types/flowStage'
 import { STATES } from 'types/SigningStates'
 import { useDisconnect } from 'wagmi'
 import AppStore from 'stores/AppStore'
 
 export default function () {
   const { disconnect } = useDisconnect()
+  const resetConnection = () => {
+    disconnect()
+    AppStore.stage = STAGE.INIT
+    AppStore.flowState = STATES.INIT
+  }
+
   return (
     <div>
       You can add another wallet’s commitment.
       <br />
-      <LinkText
-        onClick={() => {
-          AppStore.flowSucceeded = false
-          AppStore.flowState = STATES.INIT
-          disconnect()
-        }}
-      >
+      <LinkText onClick={resetConnection}>
         Disconnect wallet and add commitment for another wallet
       </LinkText>
     </div>
