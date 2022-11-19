ddb45eeb890e5593162773e472e95febc9b42419
ruslan zapevalov <ruslan@bigwhalelabs.com>
2022-11-19 07:28:46 +0400

    fix pure ip addresses

--- src/helpers/isUrl.ts
+++ src/helpers/isUrl.ts
@@ -1,5 +1,5 @@
 export const urlWithBackslash =
-  /^(http:\/\/localhost:\d+|https:\/\/[\w-]+(\.[\w-]+)+\.?(:\d+)?(?!\/))$/
+  /^(http:\/\/localhost(:\d{1,5})?|https?:\/\/[a-z0-9-]+(\.[a-z0-9-]+)*(:\d{1,5})?)$/i
 
 export default function (url: string) {
   return urlWithBackslash.test(url)
