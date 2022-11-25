87a4868611ec3cfb1b94a05c577fe5b466b46e84
Michel Erokhov <mixaileroxov@gmail.com>
2022-11-25 21:59:30 +0300

    update @big-whale-labs/seal-hub-kit

--- src/helpers/generateInput.ts
+++ src/helpers/generateInput.ts
@@ -2,10 +2,8 @@
 // Used by worker, which accepts only absolute paths, you can use this function as usual
 import { generateSignatureInputs } from '@big-whale-labs/seal-hub-kit'
 
-function generateInput(signature: string, message: string) {
+export default function generateInput(signature: string, message: string) {
   const { s, U, TPreComputes } = generateSignatureInputs(signature, message)
 
   return { s, U, TPreComputes }
 }
-
-export default generateInput
