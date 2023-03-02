a0f2c595bd58d4029841238bfd6bcab1255f24f8
smyzin <smyzin.work@gmail.com>
2023-03-02 19:34:42 +0300

    Update

--- src/helpers/resolveNetworkForEtherscan.ts
+++ src/helpers/resolveNetworkForEtherscan.ts
@@ -2,7 +2,7 @@ import * as Networks from 'wagmi/chains'
 
 export default function (chainId: number) {
   const chain = Object.values(Networks).find((chain) => chain.id === chainId)
-  return !chain || !chain.blockExplorers?.etherscan?.url
-    ? 'https://etherscan.io'
-    : chain.blockExplorers.etherscan.url
+  return chain && chain.blockExplorers?.etherscan?.url
+    ? chain.blockExplorers.etherscan.url
+    : 'https://etherscan.io'
 }
