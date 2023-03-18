5bcfef3443789838b027131110e0c20e35126505
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-03-18 13:31:14 -0700

    update to new contracts

--- src/helpers/resolveNetworkForEtherscan.ts
+++ src/helpers/resolveNetworkForEtherscan.ts
@@ -2,7 +2,11 @@ import * as Networks from 'wagmi/chains'
 
 export default function (chainId: number) {
   const chain = Object.values(Networks).find((chain) => chain.id === chainId)
-  return chain && chain.blockExplorers?.etherscan?.url
+  return chain &&
+    'blockExplorers' in chain &&
+    chain.blockExplorers &&
+    'etherscan' in chain.blockExplorers &&
+    chain.blockExplorers?.etherscan?.url
     ? chain.blockExplorers.etherscan.url
     : 'https://etherscan.io'
 }
--- src/helpers/wagmiClient.ts
+++ src/helpers/wagmiClient.ts
@@ -1,11 +1,12 @@
-import { chain, configureChains, createClient } from 'wagmi'
+import { configureChains, createClient } from 'wagmi'
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
+import { mainnet } from 'wagmi/chains'
 import { publicProvider } from 'wagmi/providers/public'
 import env from 'helpers/env'
 
 const { chains, provider } = configureChains(
-  [chain.mainnet],
+  [mainnet],
   [
     jsonRpcProvider({
       rpc: () => ({
@@ -24,6 +25,7 @@ const { connectors } = getDefaultWallets({
 export const chainList = chains
 
 export const wagmiClient = createClient({
+  autoConnect: true,
   connectors,
   provider,
 })
